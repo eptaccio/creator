5abe94afc26e51923767c456fb0e827eaf6df52a
uselessdev <wallace.oliveira@pagar.me>
2017-11-29 19:26:22 -0200

    storybook: Add preview-head.html
    Add preview-head to handle with custom font-face and reset styles on
    storybook preview

--- stories/index.js
+++ stories/index.js
@@ -4,4 +4,4 @@ import { storiesOf } from '@storybook/react'
 import Component from '../src'
 
 storiesOf('Component', module)
-  .add('with text', () => <Component>Hello World!</Component>)
+  .add('with text', () => <Component>Welcome to creator</Component>)
