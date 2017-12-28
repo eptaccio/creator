3a6e9c50c4ae741c7f48bfb5de9de0fe69c2d43b
Wallace Batista <wallace.oliveira@pagar.me>
2017-12-27 22:41:26 -0200

    eslint: add missing rules
    Add env and rules with jsx-quotes on .eslintrc.js
    
    resolve: #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ['standard', 'standard-react']
+  extends: ['standard', 'standard-react'],
+  env: {
+    jest: true
+  },
+  rules: {
+    'jsx-quotes': [2, 'preder-double']
+  }
 }
