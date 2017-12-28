6ad5d8c2c9ccc28a02c1dc8565cbf08836fdbcb7
Wallace Batista <wallace.oliveira@pagar.me>
2017-12-27 22:47:41 -0200

    bundle: add object spread plugin
    Add babel-plugin-transform-object-rest-spread to handle
    with object destructuring and spread.

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,9 @@ module.exports = {
         options: {
           presets: [
             'es2015', 'react'
+          ],
+          plugins: [
+            'transform-object-rest-spread'
           ]
         }
       },
